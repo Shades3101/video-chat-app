cd00ebedaea1d533266c6d485075640d2b013dce
Karan Narania <161560205+Shades3101@users.noreply.github.com>
2025-12-01 23:54:29 +0530

    Add OPTIONS method to CORS configuration

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -17,7 +17,7 @@ console.log("Allowed Origin:", process.env.FRONTEND_URL);
 
 app.use(cors({
     origin: [process.env.FRONTEND_URL || "", "http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
 }))
